3d7324cbd91eeeca3e7717c8060ce2357b485490
dSowaNavi <91730912+dSowaNavi@users.noreply.github.com>
2021-11-12 12:36:25 +0100

    Update s03-fizzbuzz.cpp
    std::

--- s03/s03-fizzbuzz.cpp
+++ s03/s03-fizzbuzz.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 #include <string>
 #include <cstdlib>
-using namespace std;
+
 
 int main(int argc, char* argv[])
 {
 
 	if(argc < 2 || strtof(argv[1],NULL) == 0)
 	{
-		cout << "Proszę podać argument z liczbą większą od jeden i spróbować ponownie.\n";
+		std::cout << "Proszę podać argument z liczbą większą od jeden i spróbować ponownie.\n";
 		return 1;
 
 	}	
@@ -19,10 +19,10 @@ int main(int argc, char* argv[])
 	for (int i = 1; i <= number; i++)
 	{
 
-		if (i % 3 == 0 && i % 5 == 0) { cout << i << " FizzBuzz\n";}
-		else if (i % 3 == 0) {cout << i << " Fizz\n";}
-		else if (i % 5 == 0) {cout << i << " Buzz\n";} 
-		else {cout << i << "\n";}
+		if (i % 3 == 0 && i % 5 == 0) { std::cout << i << " FizzBuzz\n";}
+		else if (i % 3 == 0) { std::cout << i << " Fizz\n";}
+		else if (i % 5 == 0) { std::cout << i << " Buzz\n";} 
+		else { std::cout << i << "\n";}
 	}	
 
 
